b8b67ad2541b27f9eece71c3d85f0fdaff4f82e9
YeemiRouth <yeemirouth@gmail.com>
2023-10-31 23:54:33 +1100

    hopefully this stops detection, its just the same as crosshair x now

--- OhShitClient/UIBase/Overlay.cs
+++ OhShitClient/UIBase/Overlay.cs
@@ -2,7 +2,6 @@
 using System.Windows.Forms;
 using System.Drawing;
 
-using static RustGame;
 using static User32;
 using static Debug;
 
@@ -10,31 +9,20 @@ class Overlay : Form
 {
     public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
 
-    public static WinEventDelegate overDel;
-
     public Overlay()
     {
-        Log("Initializing  UI..");
+        Log("Initializing UI..");
 
         InitializeComponents();
 
-        Log("Initializing  Paint Hooks..");
+        Log("Initializing Paint Hooks..");
         Paint += OnUpdate;
-
-        Log("Initializing  Delegate..");
 
         Focus(); // bring the overlay window into focus
 
         // click through
         int initStyle = GetWindowLong(Handle, -20);
         SetWindowLong(Handle, -20, initStyle | 0x80000 | 0x20);
-
-        overDel = new WinEventDelegate(OnAdjust);
-
-        Log("Initializing  WinHooks..");
-
-        SetWinEventHook((uint)SWEH_Events.EVENT_OBJECT_LOCATIONCHANGE, (uint)SWEH_Events.EVENT_OBJECT_LOCATIONCHANGE, IntPtr.Zero, overDel, GameId, GetWindowThreadProcessId(WinHandle, IntPtr.Zero), (uint)SWEH_dwFlags.WINEVENT_OUTOFCONTEXT | (uint)SWEH_dwFlags.WINEVENT_SKIPOWNPROCESS | (uint)SWEH_dwFlags.WINEVENT_SKIPOWNTHREAD);
-        SetWinEventHook((uint)SWEH_Events.EVENT_SYSTEM_FOREGROUND, (uint)SWEH_Events.EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, overDel, 0, 0, (uint)SWEH_dwFlags.WINEVENT_OUTOFCONTEXT | (uint)SWEH_dwFlags.WINEVENT_SKIPOWNPROCESS | (uint)SWEH_dwFlags.WINEVENT_SKIPOWNTHREAD);
     }
 
     public void OnUpdate(object sender, PaintEventArgs e)
@@ -60,19 +48,14 @@ class Overlay : Form
         Text = "Rust";
 
         DoubleBuffered = true; // no tearing cuz this is gay
-    }
 
-    public void OnAdjust(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
-    {
-        // adjust window (this is a hook so hopefully they dont detect it..?)
+        // fuck borderl
+        FormBorderStyle = FormBorderStyle.None;
 
-        ProcessRectangle rect = GetGameRect();
+        // maximize
+        WindowState = FormWindowState.Maximized;
 
-        int x = rect.Left; // wont do fullscreen yet
-        int y = rect.Top;
-        int width = rect.Right - rect.Left;
-        int height = rect.Bottom - rect.Top;
-
-        SetWindowPos(Handle, IsGameFocusedInsert(), x, y, width, height, 0x40); // might be working
+        // fullscreen
+        Size = Screen.PrimaryScreen.Bounds.Size;
     }
 }
