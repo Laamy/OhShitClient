2b80e083afd61f918b8f59152228ebe4e0bf2582
YeemiRouth <yeemirouth@gmail.com>
2023-10-31 23:20:34 +1100

    Add project files.

---
+++ OhShitClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // get the information we need from rust for our WinHooks
+        RustGame.OpenGame();
+
+        // just gonna start it cuz we're not injecting anything so no need for multi-thread
+        Application.Run(new Overlay());
+    }
+}
