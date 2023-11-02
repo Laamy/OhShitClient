8a73535be0659709f168d81cd9ea2f135be0d2db
YeemiRouth <yeemirouth@gmail.com>
2023-11-02 13:45:54 +1100

    idea for rust sights as crosshairs implemented

--- OhShitClient/UIBase/Overlay.cs
+++ OhShitClient/UIBase/Overlay.cs
@@ -3,6 +3,8 @@ using System.Drawing;
 
 using static User32;
 using static Debug;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Emit;
 
 class Overlay : Form
 {
@@ -31,9 +33,32 @@ class Overlay : Form
 
         Pen pen = new Pen(Color.Red, 2);
 
+        //+ crosshair
         g.DrawLine(pen, centerX - 10, centerY, centerX + 10, centerY);
-
         g.DrawLine(pen, centerX, centerY - 10, centerX, centerY + 10);
+
+        //simple mandmade sight
+        //Point imageDimsensions = new Point(309 / 8, 294 / 8);
+
+        //g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+        //using (Bitmap image = new Bitmap("Data\\Assets\\SimpleHandmadeSight.png"))
+        //{
+        //    for (int x = 0; x < image.Width; x++)
+        //    {
+        //        for (int y = 0; y < image.Height; y++)
+        //        {
+        //            Color pixel = image.GetPixel(x, y);
+
+        //            if (pixel.A < 255)
+        //                image.SetPixel(x, y, Color.Transparent); // delete bad pixels
+        //        }
+        //    }
+
+        //    Rectangle sightRect =
+        //        new Rectangle(new Point(centerX - (imageDimsensions.X / 2), centerY - (imageDimsensions.Y / 2)),
+        //        new Size(imageDimsensions.X, imageDimsensions.Y));
+        //    g.DrawImage(image, sightRect);
+        //}
     }
 
     public void InitializeComponents()
@@ -54,5 +79,17 @@ class Overlay : Form
 
         // fullscreen
         Size = Screen.PrimaryScreen.Bounds.Size;
+    }
+
+    private void InitializeComponent()
+    {
+            this.SuspendLayout();
+            // 
+            // Overlay
+            // 
+            this.ClientSize = new System.Drawing.Size(276, 240);
+            this.Name = "Overlay";
+            this.ResumeLayout(false);
+
     }
 }
